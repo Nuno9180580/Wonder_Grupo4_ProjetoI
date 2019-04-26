b3d8e3c219f70c6190fa5f6cc46ac0742d7c6d88
Joao Campos <jota.fields@gmail.com>
2019-04-26 16:39:28 +0100

    fgh

---
+++ js/models/monumentModel.js
@@ -0,0 +1,13 @@
+export default class Monument {
+    constructor(name, year, photo, description, origin) {
+
+        this.name = name
+        this.year = year
+        this.photo = photo
+        this.description = description 
+        this.origin= origin
+    }
+}
+
+
+
