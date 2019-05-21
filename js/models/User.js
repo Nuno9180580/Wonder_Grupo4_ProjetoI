646de4f6581a3c4f20bd3ed748cf9aca4770d1c1
nunof <nunofag9@hotmail.com>
2019-05-21 12:18:03 +0100

    classe utilizador criada

--- js/models/User.js
+++ js/models/User.js
@@ -2,11 +2,12 @@
  * Classe para modelar um utilizador
  */
 export default class User {
-    constructor (username, email, password, level, experience) {
+    constructor (username, email, password, level, experience, userImage) {
         this.username = username
         this.email = email
         this.password = password
         this.level = level
         this.experience = experience
+        this.userImage = userImage
     }
 }
