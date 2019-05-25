f5de3eba955e2524c65a5f814714c4b5c3c572dd
nunof <nunofag9@hotmail.com>
2019-05-25 19:10:30 +0100

    login / registo metade feito

--- js/controller/landing.js
+++ js/controller/landing.js
@@ -1,12 +1,13 @@
-//botao register para validar o registo
-const myCard = document.querySelector("#changeCard")
-//botao login para mudar o form com os campos necessarios
+import {
+  users,
+  changeCardContent,
+  registerSubmit,
+  loginSubmit
+}
+from "../models/main.js"
+
 const changeToLogin = document.querySelector("#btnChangeToLogin")
-//form
-const myForm = document.querySelector("#myForm")
-//botao registo
 const btnRegister = document.querySelector("#btnRegister")
-//botao login para efetuar o login
 const btnLogin = document.querySelector("#btnLogin")
 
 //clica no botao iniciar sessao e altera os campos do form
@@ -17,76 +18,21 @@ changeToLogin.addEventListener("click", function () {
 
 //clica e valida o registo
 btnRegister.addEventListener("click", function (event) {
-  //função que valida o registo
-  registerSubmit()
+  //obter os valores dos campos
+  const txtUsername = document.querySelector("#txtUsername").value
+  const txtEmail = document.querySelector("#txtEmail").value
+  const txtPass = document.querySelector("#txtPass").value
+  const txtConfirmPass = document.querySelector("#txtConfirmPass").value
+
+  registerSubmit(txtUsername, txtEmail, txtPass, txtConfirmPass)
+  event.preventDefault()
 })
 
-
-
-//---------------------------------------------------------//
-//--------------------FUNCTIONS----------------------------//
-//---------------------------------------------------------//
-
-//funcao para mudar os campos do card ao clicar em iniciar sessao
-function changeCardContent() {
-  //variavel que procura a card para ser alterada
-  const myCard = document.querySelector("#changeCard")
-
-  myCard.innerHTML = `
-        <div class="card-header">
-          <h3>Iniciar Sessão</h3>
-        </div>
-        <div class="card-body">
-          <form id="myForm">
-          <label for="txtUsername">Nome de Utilizador</label>
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-              </div>
-              <input type="text" class="form-control" id="txtUsername">
-            </div>
-            <label for="txtPass">Password</label>
-            <div class="input-group form-group">
-              <div class="input-group-prepend">
-              </div>
-              <input type="password" class="form-control" id="txtPass">
-            </div>            
-            <div class="form-group">
-              <input type="submit" value="Iniciar Sessão" class="btn float-left login_btn" id="btnLogin">
-            </div>
-          </form>
-        </div>
-     `
-  myCard.style.height = "330px";
-}
-
-//função para validar o registo
-function registerSubmit() {
+btnLogin.addEventListener("click", function (event) {
   //obter os valores dos campos
-  const txtUsername = document.querySelector("txtUsername").value
-  const txtEmail = document.querySelector("txtEmail").value
-  const txtPass = document.querySelector("txtPass").value
-  const txtConfirmPass = document.querySelector("txtConfirmPass").value
+  const txtUsername = document.querySelector("#txtUsername").value
+  const txtPass = document.querySelector("#txtPass").value
 
-  //se os campos nao tiverem vazios
-  if (txtUsername != "") {
-    if (txtEmail != "") {
-      if (txtPass != "") {
-
-      }
-    }
-  }
-}
-
-//função para validar o inicio de sessão
-function loginSubmit() {
-  //obter os valores dos campos
-  const txtUsername = document.querySelector("txtUsername").value
-  const txtPass = document.querySelector("txtPass").value
-
-  //se os campos nao tiverem vazios
-  if (txtUsername != "") {
-    if (txtPass != "") {
-      
-    }
-  }
-}
+  loginSubmit(txtUsername, txtPass)
+  event.preventDefault()
+})
--- js/models/Main.js
+++ js/models/Main.js
@@ -1,7 +1,7 @@
-import User from "./user.js"
+import User from "./userModel.js"
 
 //array de utilizadores 
-export const users = []
+export let users = []
 
 // Caso já exista uma chave users na LocalStorage é carregado tudo para o array
 // Caso contrário são guardadas no array, vários objetos User inseridos manualmente
@@ -9,7 +9,86 @@ export const users = []
 if (localStorage.users) {
     users = JSON.parse(localStorage.users)
 } else {
-    //const user1 = new User("admin", "admin", "admin")
-    //const user2 = new User("maria", "9876", "", "", "", "")
-    //users.push(user1, user2, user3)
+    const userAdmin = new User("admin", "admin", "admin")
+    users.push(userAdmin)
+}
+//------------------------------------------------------------------------------------------------------------------------------------//
+//---------------------------------------------------------------FUNÇÕES--------------------------------------------------------------//
+//------------------------------------------------------------------------------------------------------------------------------------//
+
+//funcao para mudar os campos do card ao clicar em iniciar sessao
+export function changeCardContent() {
+    //variavel que procura a card para ser alterada
+    const myCard = document.querySelector("#changeCard")
+
+    myCard.innerHTML = `
+        <div class="card-header">
+          <h3>Iniciar Sessão</h3>
+        </div>
+        <div class="card-body">
+          <form id="myForm">
+          <label for="txtUsername">Nome de Utilizador</label>
+            <div class="input-group form-group">
+              <div class="input-group-prepend">
+              </div>
+              <input type="text" class="form-control" id="txtUsername" placeholder="" required>
+            </div>
+            <label for="txtPass">Password</label>
+            <div class="input-group form-group">
+              <div class="input-group-prepend">
+              </div>
+              <input type="password" class="form-control" id="txtPass" placeholder="" required>
+            </div>            
+            <div class="form-group">
+              <input type="submit" value="Iniciar Sessão" class="btn float-left login_btn" id="btnLogin">
+            </div>
+          </form>
+        </div>
+     `
+    myCard.style.height = "330px";
+}
+
+//função para validar o registo para o landing
+export function registerSubmit(txtUsername, txtEmail, txtPass, txtConfirmPass) {
+    if (txtPass === txtConfirmPass) {
+        let usernameExists = false;
+        let emailExists = false;
+        for (const user of users) {
+            if (user.name === txtUsername) {
+                userExists === true;
+            } else if (user.email === txtEmail) {
+                emailExists === true;
+            }
+        }
+
+        if (usernameExists === false && emailExists === false) {
+            alert(`Conta criada com sucesso!`)
+            users.push(new User(txtUsername, txtEmail, txtPass, "1", "0", ""))
+            localStorage.setItem("users", JSON.stringify(users))
+            //redireciona para o form com o login
+            changeCardContent();
+        } else if (usernameExists === true) {
+            alert("Nome de Utilizador já existe!")
+        } else if (emailExists === true) {
+            alert("Email já existe!")
+        }
+    } else {
+        txtPass = ""
+        txtConfirmPass = ""
+        alert("As palavras passe não coincidem!")
+    }
+}
+
+//função para validar o inicio de sessão para o landing
+export function loginSubmit(txtUsername, txtPass) {
+    let userExists = false
+    for (const user of users) {
+        if (user.name === txtUsername) {
+            if (user.password === txtPass) {
+                alert(`${txtUsername} Bem-vindo!`)
+            } else {
+                alert("Password incorreta!")
+            }
+        }
+    }
 }
