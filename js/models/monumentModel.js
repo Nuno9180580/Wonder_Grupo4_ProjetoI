47e1aa325c8fc581e7866ec20a760952cc17cc11
BrunoJusta <brunomajusta@gmail.com>
2019-05-27 11:10:40 +0100

    s

--- js/models/monumentModel.js
+++ js/models/monumentModel.js
@@ -1,6 +1,5 @@
 export default class Monument {
     constructor(name, year, photo, description, city, country) {
-        this.id = id
         this.name = name
         this.year = year
         this.photo = photo
