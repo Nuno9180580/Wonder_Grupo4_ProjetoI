fbd25e6dec6d343bab29c50a60b19ee4cfa3cdef
nunof <nunofag9@hotmail.com>
2019-05-27 11:13:14 +0100

    ...

--- js/controller/landingRegister.js
+++ js/controller/landingRegister.js
@@ -13,6 +13,7 @@ const btnLogin = document.querySelector("#btnLogin")
 //clica no botao iniciar sessao e altera os campos do form
 changeToLogin.addEventListener("click", function () {
   //funcao que muda o conteudo do form para os campos necessários ao login
+  
 })
 
 //clica e valida o registo
