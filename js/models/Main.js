0929fa88deaa8574d090dcab8aea2fe2dd70d90b
nunof <nunofag9@hotmail.com>
2019-05-27 21:18:57 +0100

    login quase pronto

--- js/models/Main.js
+++ js/models/Main.js
@@ -21,36 +21,54 @@ if (localStorage.users) {
 export function registerSubmit(txtUsername, txtEmail, txtPass, txtConfirmPass) {
     let usernameExists = false
     let emailExists = false
-    if (txtPass === txtConfirmPass) {
-        for (const user of users) {
-            if (user.name === txtUsername) {
-                userExists === true
-            } else if (user.email === txtEmail) {
-                emailExists === true
+    if (txtUsername != "" && txtEmail != "" && txtPass != "" && txtConfirmPass != "") {
+        if (txtPass === txtConfirmPass) {
+            for (const user of users) {
+                if (user.username === txtUsername) {
+                    usernameExists === true
+                } else if (user.email === txtEmail) {
+                    emailExists === true
+                }
             }
-        }
-        if (usernameExists === false && emailExists === false) {
-            alert(`Conta criada com sucesso!`)
-            users.push(new User(txtUsername, txtEmail, txtPass, "1", "0", ""))
-            localStorage.setItem("users", JSON.stringify(users))
-        } else if (usernameExists === true) {
-            alert("Nome de Utilizador já existe!")
-        } else if (emailExists === true) {
-            alert("Email já existe!")
+            if (usernameExists === false && emailExists === false) {
+                alert(`Conta criada com sucesso!`)
+                users.push(new User(txtUsername, txtEmail, txtPass, "1", "0", "../img/AvatarFields.jpg"))
+                localStorage.setItem("users", JSON.stringify(users))
+                window.location.href = "../html/landingLogin.html" //redireciona para apagina login
+            }
+            if (usernameExists === true) {
+                document.querySelector("#txtUsername").value = ""
+                alert("Nome de Utilizador já existe!")
+            } else if (emailExists === true) {
+                document.querySelector("#txtEmail").value = ""
+                alert("Email já existe!")
+            }
+        } else {
+            alert("As palavras passe não coincidem!")
+            document.querySelector("#txtPass").value = ""
+            document.querySelector("#txtConfirmPass").value = ""
         }
     } else {
-        alert("As palavras passe não coincidem!")
+        alert("Preencha todos os campos!")
     }
+
 }
 
 //função para validar o inicio de sessão para o landing
 export function loginSubmit(txtUsername, txtPass) {
     let existUser = false
     for (const user of users) {
-        if (user.name === txtUsername && user.password === txtPass) {
+        if (user.username === txtUsername && user.password === txtPass) {
             sessionStorage.setItem("loggedUser", txtUsername)
             existUser = true
+            alert(`Bem-vindo ${txtUsername}!`)
+            window.location.href = "../html/index.html" //redireciona para a pagina principal
         }
+    }
+    if (existUser === false) {
+        alert("Dados incorretos!")
+        document.querySelector("#txtUsername").value = ""
+        document.querySelector("#txtPass").value = ""
     }
     return existUser
 }
