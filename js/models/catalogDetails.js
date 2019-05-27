35330358227f73c3779755f8b9bdb839efdd706d
BrunoJusta <brunomajusta@gmail.com>
2019-05-27 11:10:29 +0100

    Monuments

--- js/models/catalogDetails.js
+++
@@ -1,9 +0,0 @@
-export default class catalogDetails {
-    constructor(city, country, year, purpose, coolDetail) {
-        this.city = city
-        this.country = country
-        this.year = year
-        this.purpose = purpose
-        this.coolDetail = coolDetail
-    }
-}
--- js/models/mundoNovo.js
+++
@@ -1,9 +0,0 @@
-export default newOnes = [
-    "Grande Muralha da China",
-    "Petra",
-    "Cristo Redentor",
-    "Machu Picchu",
-    "Chichen Itza",
-    "Taj-Mahal",
-    "Coliseu de Roma"
-]
