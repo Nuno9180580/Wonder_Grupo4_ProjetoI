7c92d6f7b11043ca4707a732eac1586e2fccf375
BrunoJusta <brunomajusta@gmail.com>
2019-05-27 17:08:55 +0100

    catalog alfabeto

--- js/controller/catalog.js
+++ js/controller/catalog.js
@@ -12,8 +12,8 @@ if (localStorage.monuments) {
 } else {
     // Só vai entrar aqui a primeira vez
     const mnt1 = new Monument("Torre Eiffel", "1889", "../img/sample2.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
-    const mnt2 = new Monument("Piramides", "1889", "../img/sample2.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
-    const mnt3 = new Monument("Big Ben", "1889", "../img/sample2.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
+    const mnt2 = new Monument("Piramides de Gize", "1850 A.C", "../img/sample1.jpg", "PIRAMIDES", "Paris", "Egito")
+    const mnt3 = new Monument("Big Ben", "1889", "../img/sample.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
     const mnt4 = new Monument("Stonehendge", "1889", "../img/sample2.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
     const mnt5 = new Monument("Torre dos Clerigos", "1889", "../img/sample2.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
     const mnt6 = new Monument("Torre de Belem", "1889", "../img/sample2.jpg", "A Torre Eiffel é uma torre de ferro do século XIX e é o edifício mais alto de Paris.Nomeada em homenagem ao seu projetista, o engenheiro Gustave Eiffel.Possui 324 metros de altura e fica cerca de 15 centímetros mais alta no verão, devido à dilatação térmica do ferro.", "Paris", "França")
@@ -26,11 +26,22 @@ if (localStorage.monuments) {
 
 const myCatalog = document.querySelector("#myCatalog")
 
+
+function organize(){
+    let monuments2 = []
+    monuments2 = monuments
+    monuments2.sort((a,b) => (a.name > b.name) ? 1 : -1);
+    console.log(monuments2)
+}
+
+
+
+
+
 //carrega as bandas todas na msm sem filtros
 renderCatalog();
 //funcao para atualizar as bandas do catalogo
-function renderCatalog() 
-{
+function renderCatalog() {
     let result = "";
     let i = 0;
     for (const monument of monuments) {
@@ -39,12 +50,34 @@ function renderCatalog()
             result += `<br><div class = "row">`
         }
         //geraçao do card
-        result += ` <div class="col-sm">
+        result += ` 
+            <div class="col-sm">
                 <div class="card" style="width: 20rem;">
                     <img id="cardImg" class="card-img-top" src="${monument.photo}">
                     <div class="card-body">
                         <h5 id="cardTitle" class="card-title">${monument.name}</h5>
-                        <a href="#" class="btn btn-primary">Detalhes</a>
+                        <button type="button" class="btn btn-info btn-lg" data-toggle="modal" data-target="#myModal">Detalhes</button>                        
+                        <!-- Modal -->
+                        <div id="myModal" class="modal fade" role="dialog">
+                          <div class="modal-dialog">
+                            <div class="modal-content">
+                                <div class="modal-header">
+                                    <button type="button" class="close" data-dismiss="modal">&times;</button>
+                                    </div>
+                                    <div class="modal-body">
+                                        <h4>${monument.name}</h4>
+                                        <img id="cardImg" class="card-img-top" src="${monument.photo}">
+                                        <h5>${monument.city},${monument.country}</h5>
+                                        <h5>Ano: ${monument.year}</h5>
+                                        <h5>Detalhes:</h5>
+                                        <p>${monument.description}</p>
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                    </div>                            
+                                </div>
+                              </div>
+                        </div>
                     </div>
                 </div>
             </div>`
@@ -53,8 +86,16 @@ function renderCatalog()
             result += `</div>`
         }
     }
-    myCatalog.innerHTML =  result
+    myCatalog.innerHTML = result
 }
 
+//let monuments2 = []
+//monuments2 = monuments
+//monuments2.sort();
+//document.getElementById("myCatalog").innerHTML = monuments2;
 
-
+//function myFunction() {
+  //monuments2.sort();
+  //document.getElementById("myCatalog").innerHTML = monuments2;
+  //console.log(monuments2)
+//}
