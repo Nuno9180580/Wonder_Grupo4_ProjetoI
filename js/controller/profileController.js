f7ef147d38b46c0e9bf4ff5022a4ecf10239e10a
Joao Campos <jota.fields@gmail.com>
2019-05-31 17:04:36 +0100

    sd

--- js/controller/profileController.js
+++ js/controller/profileController.js
@@ -1,56 +1,61 @@
 import {
-  users  
-} from "../models/Main.js"
+    users  
+  } from "../models/Main.js"
+  
+  
+  const userOn = sessionStorage.getItem('loggedUser')
+  const labelUser = document.querySelector("#txtUserLogged")
+  const cardTitle = document.querySelector("#cardTitle")
+  cardTitle.innerHTML = userOn;
+  labelUser.innerHTML = userOn;
+  let imgAvatar = ""
+  let userLvl2 = ""
+  let userXP = ""
+  let email = ""
+  let pass = ""
+  for (const user of users) {
+      if (user.username === userOn) {
+          imgAvatar = user.userImage
+          userLvl2 = user.level
+          userXP = user.experience
+          email = user.email
+          pass = user.password
+  
+      }
+  }
+  const userAvatar = document.querySelector("#userAvatar")
+  const userLvl = document.querySelector("#currentLvl")
+  const userExp = document.querySelector("#xpBar")
+  const userImg = document.querySelector("#cardImg")
+  const userEmail = document.querySelector("#emailAdress")
+  const userPass = document.querySelector("#passWord")
 
-
-const userOn = sessionStorage.getItem('loggedUser')
-const labelUser = document.querySelector("#txtUserLogged")
-const cardTitle = document.querySelector("#cardTitle")
-cardTitle.innerHTML = userOn;
-labelUser.innerHTML = userOn;
-let imgAvatar = ""
-let userLvl2 = ""
-let userXP = ""
-for (const user of users) {
-    if (user.username === userOn) {
-        imgAvatar = user.userImage
-        userLvl2 = user.level
-        userXP = user.experience
-
+  
+  
+  
+  function xpBarColor(){
+    if(userXP <= 33)
+    {
+      userExp.style.backgroundColor = "red" ;
+      
     }
-}
-const userAvatar = document.querySelector("#userAvatar")
-const userLvl = document.querySelector("#currentLvl")
-const userExp = document.querySelector("#xpBar")
-const userImg = document.querySelector("#cardImg")
-
-
-
-function xpBarColor(){
-  if(userXP <= 33)
-  {
-    userExp.style.backgroundColor = "red" ;
-    
+    else if(userXP >= 33 && userXP <= 66){
+      userExp.style.backgroundColor = "#ffa812" ;
+  
+    }
+    else{
+    userExp.style.backgroundColor = "#0aac20" ;
+    }
   }
-  else if(userXP >= 33 && userXP <= 66){
-    userExp.style.backgroundColor = "#ffa812" ;
-
-  }
-  else{
-  userExp.style.backgroundColor = "#0aac20" ;
-  }
-}
-
-xpBarColor();
-userImg.src = imgAvatar
-userExp.innerHTML = userXP
-userExp.style.width = userXP +"%";
-userAvatar.src = imgAvatar
-userLvl.innerHTML = "Nível: " + userLvl2
-
-
-const btnsSeeMore = document.getElementsByClassName("view")
-btnsSeeMore.addEventListener("click", function () {})
-
-
-
+  
+  xpBarColor();
+  userImg.src = imgAvatar
+  userExp.innerHTML = userXP
+  userExp.style.width = userXP +"%";
+  userAvatar.src = imgAvatar
+  userLvl.innerHTML = "Nível: " + userLvl2
+  userEmail.value = email;
+  userPass.value = pass;
+  
+  const btnsSeeMore = document.getElementsByClassName("view")
+  btnsSeeMore.addEventListener("click", function () {})
