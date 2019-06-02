a9a3ad9ccfe3d4baa9bc06cee455db6d1b4082c5
nunof <nunofag9@hotmail.com>
2019-06-02 18:31:31 +0100

    quizz array temporario criado

--- js/controller/quizzController.js
+++ js/controller/quizzController.js
@@ -5,6 +5,7 @@ from "../models/Main.js"
 import {
     questions
 } from "../views/quizView.js"
+import Question from "../models/questionModel.js"
 
 //avatar e nome da pessoa logada
 const userOn = sessionStorage.getItem('loggedUser')
@@ -33,4 +34,30 @@ const quizLevel = document.querySelector("#quizLevel")
 const counter = document.querySelector("#counter")
 const fillTimeBar = document.querySelector("#barFilling")
 
-let numberOfQuestions = questions.length -1;
+//obter o nivel do utilizador para corresponder ao niveis das perguntas
+let userLevel = ""
+for (const user of users) {
+    if (user.username === userOn) {
+        console.log(user.level)
+        userLevel = user.level
+    }
+}
+//criar array temporario para as questions do nivel do user
+let TempQuestions = []
+for (const question of questions) {
+    if (question.level === userLevel) {
+        TempQuestions.push(new Question(question.question, question.imgQuestion, question.choiceA, question.choiceB, question.choiceC, question.choiceD, question.correct, question.level))
+        localStorage.setItem("questions", JSON.stringify(TempQuestions))
+    }
+}
+
+//CHAMA UMA QUESTAO ALEATORIA
+/* let range = TempQuestions.length - 1;
+let randomIndex = Math.floor((Math.random() * range) + 0); */
+let numberOfQuestions = questions.length - 1;
+
+//1. buscar um monumento random
+//2. ler a imagem do monumento
+//3. vai buscar uma pergunta random
+//4. substitui o identificador da resposta e imagem pelo respetivo monumento
+//perguntar ao stor os passos para o quiz
