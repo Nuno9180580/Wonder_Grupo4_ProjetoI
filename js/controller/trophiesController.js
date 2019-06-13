6b8c729c63eb6bd25b9e2e87d34f5ca2e922fd49
nunof <nunofag9@hotmail.com>
2019-06-13 17:37:20 +0100

    TrOPHIE UPDATE

--- js/controller/trophiesController.js
+++ js/controller/trophiesController.js
@@ -10,6 +10,7 @@ const userOn = sessionStorage.getItem('loggedUser')
 
 document.querySelector("#btn1").addEventListener("click", function () {
     const img = document.querySelector("#img1")
+    const changeBorder = document.querySelector(".card")
     let XP = ""
     for (const user of users) {
         if (user.username === userOn) {
@@ -18,24 +19,13 @@ document.querySelector("#btn1").addEventListener("click", function () {
     }
     if (XP >= 100) {
         img.src = "../img/medal1.png"
-    }
-})
-
-document.querySelector("#btn5").addEventListener("click", function () {
-    const img = document.querySelector("#img5")
-    let XP = ""
-    for (const user of users) {
-        if (user.username === userOn) {
-            XP = user.experience
-        }
-    }
-    if (XP >= 500) {
-        img.src = "../img/medal1.png"
+        changeBorder.style.border = "3px solid #eb2020";
     }
 })
 
 document.querySelector("#btn2").addEventListener("click", function () {
     const img = document.querySelector("#img2")
+    const changeBorder = document.querySelector(".card")
     let XP = ""
     for (const user of users) {
         if (user.username === userOn) {
@@ -44,11 +34,13 @@ document.querySelector("#btn2").addEventListener("click", function () {
     }
     if (XP >= 200) {
         img.src = "../img/medal2.png"
+        changeBorder.style.border = "3px solid #eb2020";
     }
 })
 
 document.querySelector("#btn3").addEventListener("click", function () {
     const img = document.querySelector("#img3")
+    const changeBorder = document.querySelector(".card")
     let XP = ""
     for (const user of users) {
         if (user.username === userOn) {
@@ -57,11 +49,13 @@ document.querySelector("#btn3").addEventListener("click", function () {
     }
     if (XP >= 300) {
         img.src = "../img/medal3.png"
+        changeBorder.style.border = "3px solid #eb2020";
     }
 })
 
 document.querySelector("#btn4").addEventListener("click", function () {
     const img = document.querySelector("#img4")
+    const changeBorder = document.querySelector(".card")
     let XP = ""
     for (const user of users) {
         if (user.username === userOn) {
@@ -70,9 +64,39 @@ document.querySelector("#btn4").addEventListener("click", function () {
     }
     if (XP >= 400) {
         img.src = "../img/medal4.png"
+        changeBorder.style.border = "3px solid #eb2020";
     }
 })
 
+document.querySelector("#btn5").addEventListener("click", function () {
+    const img = document.querySelector("#img5")
+    const changeBorder = document.querySelector(".card")
+    let XP = ""
+    for (const user of users) {
+        if (user.username === userOn) {
+            XP = user.experience
+        }
+    }
+    if (XP >= 500) {
+        img.src = "../img/medal1.png"
+        changeBorder.style.border = "3px solid #eb2020";
+    }
+})
+
+document.querySelector("#btn6").addEventListener("click", function () {
+    const img = document.querySelector("#img6")
+    const changeBorder = document.querySelector(".card")
+    let XP = ""
+    for (const user of users) {
+        if (user.username === userOn) {
+            XP = user.experience
+        }
+    }
+    if (XP === 1000) {
+        img.src = "../img/medal1.png"
+        changeBorder.style.border = "3px solid #eb2020";
+    }
+})
 
 /* ---------------------------------------------------------------------Funções--------------------------------------------------------*/
 
