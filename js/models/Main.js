6179ebf89a666500450dbc3559db5189335a6cb6
BrunoJusta <brunomajusta@gmail.com>
2019-06-19 12:09:00 +0100

    error fixed

--- js/models/Main.js
+++ js/models/Main.js
@@ -56,7 +56,8 @@ const dummyMonuments = [{
     description: "Apesar do termo também ser usado para se referir à torre do relógio onde o sino está localizado, a estrutura é oficialmente conhecida como a Elizabeth Tower, rebatizada para comemorar o Jubileu de Diamante da Rainha Isabel II do Reino Unido. A edificação possui o segundo maior relógio de quatro faces do mundo, foi construída em estilo neogótico e tem 96 metros de altura. A torre foi concluída em 1858 e tornou-se um dos símbolos mais importantes do Reino Unido.",
     city: "Londres",
     country: "Reino Unido",
-    level: "1"
+    level: "1",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Torre de Belém",
@@ -65,7 +66,8 @@ const dummyMonuments = [{
     description: "A Torre de Belém, oficialmente Torre de São Vicente, localiza-se na freguesia de Belém, concelho e distrito de Lisboa, em Portugal. Na margem direita do rio Tejo, onde existiu outrora a praia de Belém, era primitivamente cercada pelas águas em todo o seu perímetro. Ao longo dos séculos foi envolvida pela praia, até se incorporar hoje a terra firme. Um dos ex libris da cidade, o monumento é um ícone da arquitetura do reinado de D. Manuel I, numa síntese entre a torre de menagem de tradição medieval e o baluarte moderno, onde se dispunham peças de artilharia.",
     city: "Lisboa",
     country: "Portugal",
-    level: "1"
+    level: "1",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Estátua da Liberdade",
@@ -74,7 +76,8 @@ const dummyMonuments = [{
     description: "A estátua de cobre, projetada pelo escultor francês Frédéric Auguste Bartholdi, que se baseou no Colosso de Rodes, foi construída por Gustave Eiffel. Foi um presente dado aos Estados Unidos pelo povo da França. A estátua é de uma figura feminina vestida que representa Libertas, deusa romana, que carrega uma tocha e um tabula ansata (uma tabuleta que evoca uma lei) sobre a qual está inscrita a data da Declaração da Independência dos Estados Unidos, 4 de julho de 1776. Uma corrente quebrada encontra-se a sob pés. A estátua é um ícone da liberdade e dos Estados Unidos, além de ser um símbolo de boas-vindas aos imigrantes que chegam do exterior.",
     city: "New York",
     country: "E.U.A.",
-    level: "1"
+    level: "1",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Palácio da Pena",
@@ -83,7 +86,8 @@ const dummyMonuments = [{
     description: "Localiza-se na vila de Sintra, freguesia de São Pedro de Penaferrim, concelho de Sintra, no distrito de Lisboa. Representa uma das principais expressões do Romantismo arquitectónico do século XIX no mundo, constituindo-se no primeiro palácio nesse estilo na Europa, erguido cerca de 30 anos antes do Castelo de Neuschwanstein, na Baviera. Em 7 de julho de 2007 foi eleito como uma das Sete Maravilhas de Portugal.",
     city: "Sintra",
     country: "Portugal",
-    level: "1"
+    level: "1",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Stonehenge",
@@ -92,7 +96,8 @@ const dummyMonuments = [{
     description: "Estrutura formada por círculos concêntricos de pedras que chegam a ter 5 metro de altura e a pesar quase 50 toneladas. Existem diversas lendas e mitos acerca da sua construção, creditada a diversos povos da Antiguidade.Encontram-se 3 períodos distintos de construção.",
     city: "Salisbury",
     country: "Inglaterra",
-    level: "2"
+    level: "2",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Cristo Redentor",
@@ -101,7 +106,8 @@ const dummyMonuments = [{
     description: "Estátua que retrata Jesus Cristo, localizada no topo do morro do Corcovado, a 709 metros acima do nível do mar, no Parque Nacional da Tijuca, com vista para a cidade do Rio de Janeiro. Símbolo do cristianismo brasileiro.  Em 2007 foi eleito como uma das sete maravilhas do mundo moderno. ",
     city: "Rio de Janeiro",
     country: "Brasil",
-    level: "2"
+    level: "2",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Torre dos Clérigos",
@@ -110,7 +116,8 @@ const dummyMonuments = [{
     description: "No ano de 1753, Nicolau Nasoni apresentou o projeto de uma torre sineira para o conjunto dos Clérigos, que haveria de substituir as duas torres inicialmente previstas para as fachadas laterais da igreja. Para além de servir como torre sineira, esta edificação teve outras utilizações ao longo dos anos: serviu para marcar o tempo (através de um disparo diário de pólvora seca que assinalava o meio dia); foi telégrafo comercial; foi utilizada como marco de orientação para as embarcações que rumavam no rio Douro, entre outros.",
     city: "Porto",
     country: "Portugal",
-    level: "2"
+    level: "2",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Muralha da China",
@@ -119,7 +126,8 @@ const dummyMonuments = [{
     description: "É uma das maravilhas do mundo moderno. Fortificações feitas de pedra, tijolo, terra, madeira e outros materiais, geralmente construída ao longo de uma linha através das fronteiras históricas do norte da China para proteger os Estados e Impérios chineses principalmente contra as invasões dos mongóis. Os próprios operários, quando morriam, eram usados como material para construir a muralha.",
     city: "China",
     country: "China",
-    level: "2"
+    level: "2",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Coliseu de Roma",
@@ -128,7 +136,8 @@ const dummyMonuments = [{
     description: "Construído com concreto e areia, é o maior anfiteatro já construído e está situado a leste do Fórum Romano. O Coliseu poderia abrigar entre 50 mil e 80 mil espectadores, com uma audiência média de cerca de 65 mil pessoas. O edifício era usado para combates de gladiadores e espetáculos públicos, tais como simulações de batalhas marítimas ( era inundado através de certos mecanismos), execuções, encenações de batalhas famosas e dramas baseados na mitologia clássica.",
     city: "Roma",
     country: "Itália",
-    level: "2"
+    level: "2",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Torre de Pisa",
@@ -137,7 +146,8 @@ const dummyMonuments = [{
     description: "É uma torre sineira da catedral da cidade italiana de Pisa. Está situada atrás da catedral e é a terceira estrutura mais antiga na praça da Catedral de Pisa. Embora destinada a ficar na vertical, a torre começou a inclinar-se para sudeste logo após o início da construção devido a uma fundação mal construída e a um solo mal consolidado",
     city: "Pisa",
     country: "Itália",
-    level: "2"
+    level: "2",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Notre-Dame",
@@ -146,7 +156,8 @@ const dummyMonuments = [{
     description: "É uma das mais antigas catedrais francesas de estilo gótico. Iniciada sua construção no ano de 1163, é dedicada à Virgem Maria, mãe de Jesus. Em 15 de abril de 2019 a catedral foi atingida por um violento incêndio causando danos ao teto, pináculo e rosáceas.",
     city: "Paris",
     country: "França",
-    level: "3"
+    level: "3",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Grande Esfinge",
@@ -155,7 +166,8 @@ const dummyMonuments = [{
     description: "É uma estátua que representa uma esfinge (uma criatura mítica com corpo de leão eccabeça humana) localizada no planalto de Gizé, na margem oeste do rio Nilo. Construído para o faraó Quéfren, este é o monumento monolítico mais antigo que nós conhecemos. Segundo a lenda, a criatura mítica propunha este enigma a que se cruzasse com ela: Que criatura tem quatro pernas de manhã, à tarde tem duas, e à noite tem três? Quem falhasse era devorado pela esfinge.",
     city: "Cairo",
     country: "Egipto",
-    level: "3"
+    level: "3",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Templo de Diana",
@@ -164,7 +176,8 @@ const dummyMonuments = [{
     description: "Este monumento faz parte do centro histórico da cidade de Évora. Foi classificado como Patrimônio Mundial pela UNESCO. É um dos marcos mais famosos da cidade e um dos símbolos mais relevantes da presença romana em território português.",
     city: "Évora",
     country: "Portugal",
-    level: "3"
+    level: "3",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Burj Khalifa",
@@ -173,7 +186,8 @@ const dummyMonuments = [{
     description: "É de momento o maior arranha-céus construído por nós, com 828 metros de altura e 160 andares. O orçamento total do projeto girou á volta de 1,5 biliões de dólares!",
     city: "Dubai",
     country: "Emirados Árabes Unidos",
-    level: "3"
+    level: "3",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Colosso de Ródes",
@@ -182,7 +196,8 @@ const dummyMonuments = [{
     description: "Foi uma das sete maravilhas do mundo antigo. Era uma estátua do deus grego do Sol, Hélios. Foi construído por Carés de Lindos para comemorar a vitória de Rodes contra os macedónios e, com 33 metros de altura, era considerado uma das mais altas estátuas do mundo antigo. Mais tarde destruído por um terramoto...",
     city: "Rodes",
     country: "Grécia",
-    level: "3"
+    level: "3",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Taj Mahal",
@@ -191,7 +206,8 @@ const dummyMonuments = [{
     description: "Anunciado em 2007 como uma das maravilhas do mundo moderno. A obra foi feita entre 1632 e 1653 com a força de cerca de 20 mil homens. O imperados Shah Jahan mandou construir este monumento de mármore branco em memória à sua esposa favorita, Aryumand Banu Begam. Ela morreu após dar à luz o 14º filho, tendo o Taj Mahal sido construído sobre seu túmulo, junto ao rio Yamuna. Simboliza amor e paixão.",
     city: "Agra",
     country: "Índia",
-    level: "3"
+    level: "3",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Machu Picchu",
@@ -200,7 +216,8 @@ const dummyMonuments = [{
     description: " também chamada a Cidade Perdida dos Incas, Machu Picchu é uma cidade pré-colombiana bem conservada, localizada no topo de uma montanha, a 2400 metros de altitude, no vale do rio Urubamba, atual Peru.",
     city: "Keystone",
     country: "Estados Unidos da América",
-    level: "4"
+    level: "4",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Coliseu de Roma",
@@ -209,7 +226,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Roma",
     country: "Itália",
-    level: "4"
+    level: "4",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Palácio da Pena",
@@ -218,7 +236,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Sintra",
     country: "Portugal",
-    level: "4"
+    level: "4",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Monte Rushmore",
@@ -227,7 +246,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Keystone",
     country: "Estados Unidos da América",
-    level: "4"
+    level: "4",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Coliseu de Roma",
@@ -236,7 +256,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Roma",
     country: "Itália",
-    level: "4"
+    level: "4",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Palácio da Pena",
@@ -245,7 +266,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Sintra",
     country: "Portugal",
-    level: "4"
+    level: "4",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Monte Rushmore",
@@ -254,7 +276,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Keystone",
     country: "Estados Unidos da América",
-    level: "5"
+    level: "5",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Coliseu de Roma",
@@ -263,7 +286,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Roma",
     country: "Itália",
-    level: "5"
+    level: "5",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Palácio da Pena",
@@ -272,7 +296,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Sintra",
     country: "Portugal",
-    level: "5"
+    level: "5",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Monte Rushmore",
@@ -281,7 +306,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Keystone",
     country: "Estados Unidos da América",
-    level: "5"
+    level: "5",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Coliseu de Roma",
@@ -290,7 +316,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Roma",
     country: "Itália",
-    level: "5"
+    level: "5",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   },
   {
     name: "Palácio da Pena",
@@ -299,7 +326,8 @@ const dummyMonuments = [{
     description: "Lorem Ipsum",
     city: "Sintra",
     country: "Portugal",
-    level: "5"
+    level: "5",
+    linkMap: "https://goo.gl/maps/tyytB5PBmtrMYQCLA"
   }
 ];
 
